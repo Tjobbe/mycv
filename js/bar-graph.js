80a552e08f688639d8d38a7bf865731b164ed850
tjobbe <tjobbe@gmail.com>
2013-11-06 08:35:36 +0000

    Amends, typos and changes to bar graph values

--- js/bar-graph.js
+++ js/bar-graph.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('#skillSet li').each(function(){ //for each li
-		percentage = $(this).attr('data-experience'); //get the relevant data-attr val
+		percentage = $(this).attr('data-percentage'); //get the relevant data-attr val
 		$(this).css({
 			'width': percentage+'%' //and set the width of the li accordingly
 		});
